8506f9363ba05722b98f3398226b8d75b650b3c2
Anderson Lemke <lemke@taller.net.br>
2022-04-06 09:53:17 -0300

    feat: builded index layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script context="module">
+  /** @type {import('./index').Load} */
   export async function load() {
     const today = new Date().toISOString().split('T')[0]
     const url = `https://v3.football.api-sports.io/fixtures/${today}`
@@ -15,9 +16,50 @@
 
 <script>
   import { onMount } from 'svelte'
+  let matches = [
+    {
+      time: '08:00',
+      homeTeam: 'São Paulo',
+      awayTeam: 'Parmera'
+    }
+  ]
   onMount(() => {
     load()
   })
 </script>
 
-<h1>Brasileirão</h1>
+<template>
+  <body>
+    <div class="container">
+      <p />
+      <div class="row">
+        <div class="four columns">
+          <h3>Brasileirão</h3>
+        </div>
+        <div class="eight columns">
+          <input type="date" />
+          <input class="button-primary" type="submit" value="Pesquisar" />
+        </div>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Horário</th>
+            <th>Casa</th>
+            <th>Fora</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each matches as match}
+            <tr>
+              <td>{match.time}</td>
+              <td>{match.homeTeam}</td>
+              <td>{match.awayTeam}</td>
+            </tr>
+          {/each}
+          <tr />
+        </tbody>
+      </table>
+    </div>
+  </body>
+</template>

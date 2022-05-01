3216acb8b68660a45a338a678dd0d39d117a9f2e
Anderson Lemke <lemke@taller.net.br>
2022-05-01 17:56:09 -0300

    feat: add constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,13 @@
+export const countries = [
+  { name: 'Brasil', code: 'BR' },
+  { name: 'Mundo', code: 'W' }
+]
+
+export const leagues = [
+  { id: 2, name: 'Champions League', countryCode: 'W' },
+  { id: 11, name: 'Sulamericana', countryCode: 'W' },
+  { id: 13, name: 'Libertadores', countryCode: 'W' },
+  { id: 71, name: 'Brasileirão - A', countryCode: 'BR' },
+  { id: 72, name: 'Brasileirão - B', countryCode: 'BR' },
+  { id: 73, name: 'Copa do Brasil', countryCode: 'BR' }
+]

616adb782efa423bc14d687ca8c43b4ebb1b03c7
Anderson Lemke <lemke@taller.net.br>
2022-05-29 13:33:12 -0300

    feat: change icons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,6 @@
   import { FixtureMapper } from '$lib/mapper/FixtureMapper'
   import { LeagueMapper } from '../lib/mapper/LeagueMapper'
   import { SvelteToast, toast } from '@zerodevx/svelte-toast'
-  import { FaRegTrashAlt, FaAngleRight } from 'svelte-icons/fa'
 
   let fixtures: Fixture[] = []
   let date = new Date().toISOString().split('T')[0]
@@ -88,7 +87,7 @@
         }
       }
     } catch (error) {
-      console.log('error', error)
+      toast.push(error)
     }
   }
 </script>
@@ -107,7 +106,17 @@
           <div slot="header" class="header row">
             <div class="one column">
               <div class="fan-right-icon">
-                <FaAngleRight />
+                <svg
+                  style="tran"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M9 5l7 7-7 7" />
+                </svg>
               </div>
             </div>
             <div class="ten columns">
@@ -116,7 +125,11 @@
             <div class="one column">
               <button class="trash-icon">
                 <div>
-                  <FaRegTrashAlt />
+                  <svg class="svg-icon" viewBox="0 0 20 20">
+                    <path
+                      d="M17.114,3.923h-4.589V2.427c0-0.252-0.207-0.459-0.46-0.459H7.935c-0.252,0-0.459,0.207-0.459,0.459v1.496h-4.59c-0.252,0-0.459,0.205-0.459,0.459c0,0.252,0.207,0.459,0.459,0.459h1.51v12.732c0,0.252,0.207,0.459,0.459,0.459h10.29c0.254,0,0.459-0.207,0.459-0.459V4.841h1.511c0.252,0,0.459-0.207,0.459-0.459C17.573,4.127,17.366,3.923,17.114,3.923M8.394,2.886h3.214v0.918H8.394V2.886z M14.686,17.114H5.314V4.841h9.372V17.114z M12.525,7.306v7.344c0,0.252-0.207,0.459-0.46,0.459s-0.458-0.207-0.458-0.459V7.306c0-0.254,0.205-0.459,0.458-0.459S12.525,7.051,12.525,7.306M8.394,7.306v7.344c0,0.252-0.207,0.459-0.459,0.459s-0.459-0.207-0.459-0.459V7.306c0-0.254,0.207-0.459,0.459-0.459S8.394,7.051,8.394,7.306"
+                    />
+                  </svg>
                 </div>
               </button>
             </div>
@@ -167,15 +180,16 @@
       margin: 0;
     }
 
-    input[type='submit'] {
+    button[type='submit'] {
       margin: 0;
       margin-left: 15px;
     }
   }
 
-  .fan-right-icon {
+  div.fan-right-icon {
     height: 36px;
-    width: auto;
+    width: 36px;
+    padding-top: 5px;
   }
 
   button.trash-icon {
